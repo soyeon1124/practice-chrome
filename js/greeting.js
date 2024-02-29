d55724935db5f25817c9ad4896703ab109c73d20
Soyeon <sy623762@gmail.com>
2024-02-29 20:06:23 +0900

    Timeouts and Dates

---
+++ js/greeting.js
@@ -0,0 +1,32 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event)
+    {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    painGreetings(SavedUsername);
+    }
+    
+function painGreetings(username) {
+    greeting.innerText = `Hello ${SavedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const SavedUsername = localStorage.getItem(USERNAME_KEY);
+
+// 만약 SavedUsernamer 값이 null이라면 form의 hidden class명을 지워줌
+if (SavedUsername === null) {
+loginForm.classList.remove(HIDDEN_CLASSNAME);
+loginForm.addEventListener("submit", onLoginSubmit);
+} 
+// greeting안에 텍스트추가, greeting한테서 HIDDEN_CLASSNAME 제거
+else {
+painGreetings(SavedUsername);
+}
